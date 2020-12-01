c75ff6c594c08f3ebbfd4273ee75f3731c7037a6
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-12-01 09:13:29 +0700

    update

--- ProductManager/Models/Product.cs
+++ ProductManager/Models/Product.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ProductManager.Models
@@ -10,6 +12,11 @@ namespace ProductManager.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+
+        [JsonPropertyName("img")]
         public string Image { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
     }
 }
