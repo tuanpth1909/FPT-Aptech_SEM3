0632e0e29b333bd0fa178b32145e5bd7922cc2b9
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-12-01 08:59:41 +0700

    update

--- ProductManager/Pages/Index.cshtml.cs
+++ ProductManager/Pages/Index.cshtml.cs
@@ -5,21 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ProductManager.Models;
+using ProductManager.Services;
 
 namespace ProductManager.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+            private readonly ILogger<IndexModel> _logger;
+            public JsonFileProductService ProductService;
+            public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+            public IndexModel(
+                ILogger<IndexModel> logger,
+                JsonFileProductService productService
+                )
+            {
+                _logger = logger;
+                ProductService = productService;
+            }
 
-        public void OnGet()
-        {
-
-        }
+            public void OnGet()
+            {
+                Products = ProductService.GetProducts();
+            }
     }
 }
